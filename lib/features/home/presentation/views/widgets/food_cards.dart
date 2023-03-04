b02c1f2d1aeef692bbebde648f83bc5245aaf8d5
Ahmed Safwat <a01273477051@gmail.com>
2023-03-04 19:59:49 +0200

    create the card's image and description

--- lib/features/home/presentation/views/widgets/food_cards.dart
+++ lib/features/home/presentation/views/widgets/food_cards.dart
@@ -1,17 +1,53 @@
 import 'package:flutter/material.dart';
 
 class FoodCards extends StatelessWidget {
-  const FoodCards({super.key, required this.index});
+  const FoodCards({super.key, this.index});
+  final int? index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      CardsImage(
+        index: index!,
+      ),
+      const CardsText()
+    ]);
+  }
+}
+
+class CardsImage extends StatelessWidget {
+  const CardsImage({super.key, required this.index});
   final int index;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 220,
-      margin: const EdgeInsets.symmetric(horizontal: 5),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color: const Color(0xff69c5df),
+        height: 220,
+        margin: const EdgeInsets.symmetric(horizontal: 5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color:
+              index.isEven ? const Color(0xff69c5df) : const Color(0xff929fcc),
+          image: const DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage('assets/image/food1.jpg'),
+          ),
+        ));
+  }
+}
+
+class CardsText extends StatelessWidget {
+  const CardsText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: 150,
+        margin: const EdgeInsets.only(left: 30, right: 30, bottom: 15),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30), color: Colors.blue),
       ),
     );
   }
--- lib/features/home/presentation/views/widgets/food_cards_pageview.dart
+++ lib/features/home/presentation/views/widgets/food_cards_pageview.dart
@@ -10,11 +10,15 @@ class FoodCardsPageView extends StatefulWidget {
 }
 
 class _FoodCardsPageViewState extends State<FoodCardsPageView> {
+  PageController pageController = PageController(viewportFraction: 0.85);
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 320,
+      color: Colors.red,
       child: PageView.builder(
+        controller: pageController,
+        physics: const BouncingScrollPhysics(),
         itemCount: 5,
         itemBuilder: (context, position) {
           return FoodCards(

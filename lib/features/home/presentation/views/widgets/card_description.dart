f10c147fe4cd17a090b2e0f1cbe731b2683601c1
Ahmed Safwat <a01273477051@gmail.com>
2023-03-05 16:50:19 +0200

    add shadow under the description section

--- lib/features/home/presentation/views/widgets/card_description.dart
+++ lib/features/home/presentation/views/widgets/card_description.dart
@@ -15,7 +15,24 @@ class CardsDescription extends StatelessWidget {
         height: 120,
         margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30), color: Colors.white),
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
+          boxShadow: const [
+            BoxShadow(
+              color: Color(0xffe8e8e8),
+              blurRadius: 5,
+              offset: Offset(0, 5),
+            ),
+            BoxShadow(
+              color: Colors.white,
+              offset: Offset(-5, 0),
+            ),
+            BoxShadow(
+              color: Colors.white,
+              offset: Offset(5, 0),
+            )
+          ],
+        ),
         child: Container(
             padding: const EdgeInsets.only(top: 10, left: 15, right: 15),
             child: Column(
